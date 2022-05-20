835e1627645feb6301d7d773b988a110045a2f33
patrick-n4 <patrickndayambaje4@gmail.com>
2022-05-20 14:09:28 +0200

    This is a change made

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Footer() {
-  return <div className="bg-[#FF8C1A] h-[60vh] w-[100%]"></div>;
+export default function Footer({logo}) {
+  return <div className="bg-[#FF8C1A] h-[60vh] w-[100%] flex flex-row justify-evenly items-center">
+    <div className="bg-red-400 h-[70%] w-[20%]">
+      <img src={logo} alt="" />
+    </div>
+    <div className="bg-red-400 h-[70%] w-[20%]"></div>
+    <div className="bg-red-400 h-[70%] w-[20%]"></div>
+    <div className="bg-red-400 h-[70%] w-[20%]"></div>
+  </div>;
 }

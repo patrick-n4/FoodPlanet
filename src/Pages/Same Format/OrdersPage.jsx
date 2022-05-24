ccb59e653213e61590acccc1181709a37e827600
patrick-n4 <patrickndayambaje4@gmail.com>
2022-05-24 13:28:08 +0200

    This is it make the page correct as in the design mellisa this is not what we want

--- src/Pages/Same Format/OrdersPage.jsx
+++ src/Pages/Same Format/OrdersPage.jsx
@@ -3,53 +3,86 @@ import TopNav from "../../Components/TopNav";
 
 export default function Order() {
   return (
-    <div className="flex flex-col w-[80%] h-[100%] orders-page ml-[15%]">
+    <div className="flex flex-col w-[80%] h-[100%] orders-page">
       <TopNav title="Orders" />
       <div id="stats-order-card">
-        
         <div id="stats">
-          <h2>Total Orders</h2><h4>13</h4>
-          <h2>Delivered</h2><h4>13</h4>
-          <h2>Pending</h2><h4>0</h4>
+          <h2>Total Orders</h2>
+          <h4>13</h4>
+          <h2>Delivered</h2>
+          <h4>13</h4>
+          <h2>Pending</h2>
+          <h4>0</h4>
         </div>
-        
-      <div id="new-order">
-        <h3 className="text-[1em] order-card-title">New Order</h3>
-        <div className="order-items flex flex-row items-center gap-[1em]">
-          <input type="checkbox" name="Appetizer" id="appetizer" className="order w-[1.5em] h-[2em] " />
-          <span className="order-items text-[1em] font-medium">Appetizer</span>
-          <button className="newitem">+ New</button>
-        </div>
-        <div className="order-items flex flex-row items-center gap-[1em]">
-          <input type="checkbox" name="Starter" id="starter" className="order w-[1.5em] h-[2em] " />
-          <span className="order-items text-[1em] font-medium">Starter</span>
-          <button className="newitem">+ New</button>
-        </div>
-        <div className="order-items flex flex-row items-center gap-[1em]">
-          <input type="checkbox" name="Main" id="main-meal" className="order w-[1.5em] h-[2em] " />
-          <span className="order-items text-[1em] font-medium">Main</span>
-          <button className="newitem">+ New</button>
-        </div>
-        <div className="order-items flex flex-row items-center gap-[1em]">
-          <input type="checkbox" name="Dessert" id="dessert" className="order w-[1.5em] h-[2em] " />
-          <span className="order-items text-[1em] font-medium">Dessert</span>
-          <button className="newitem">+ New</button>
-        </div>
-        <div className="order-items flex flex-row items-center gap-[1em]">
-          <input type="checkbox" name="Drink" id="drink" className="order w-[1.5em] h-[2em] " />
-          <span className="order-items text-[1em] font-medium">Drink</span>
-          <button className="newitem">+ New</button>
-        </div>
+
+        <div id="new-order">
+          <h3 className="text-[1em] order-card-title">New Order</h3>
+          <div className="order-items flex flex-row items-center gap-[1em]">
+            <input
+              type="checkbox"
+              name="Appetizer"
+              id="appetizer"
+              className="order w-[1.5em] h-[2em] "
+            />
+            <span className="order-items text-[1em] font-medium">
+              Appetizer
+            </span>
+            <button className="newitem">+ New</button>
+          </div>
+          <div className="order-items flex flex-row items-center gap-[1em]">
+            <input
+              type="checkbox"
+              name="Starter"
+              id="starter"
+              className="order w-[1.5em] h-[2em] "
+            />
+            <span className="order-items text-[1em] font-medium">Starter</span>
+            <button className="newitem">+ New</button>
+          </div>
+          <div className="order-items flex flex-row items-center gap-[1em]">
+            <input
+              type="checkbox"
+              name="Main"
+              id="main-meal"
+              className="order w-[1.5em] h-[2em] "
+            />
+            <span className="order-items text-[1em] font-medium">Main</span>
+            <button className="newitem">+ New</button>
+          </div>
+          <div className="order-items flex flex-row items-center gap-[1em]">
+            <input
+              type="checkbox"
+              name="Dessert"
+              id="dessert"
+              className="order w-[1.5em] h-[2em] "
+            />
+            <span className="order-items text-[1em] font-medium">Dessert</span>
+            <button className="newitem">+ New</button>
+          </div>
+          <div className="order-items flex flex-row items-center gap-[1em]">
+            <input
+              type="checkbox"
+              name="Drink"
+              id="drink"
+              className="order w-[1.5em] h-[2em] "
+            />
+            <span className="order-items text-[1em] font-medium">Drink</span>
+            <button className="newitem">+ New</button>
+          </div>
         </div>
       </div>
-    
+
       <div id="order-bar">
         <h2>Orders</h2>
         <button className="filter-order">Delivered</button>
-        <button className="filter-order" id="pend">Pending</button>
-        <button className="filter-order">All</button><br /><br />
+        <button className="filter-order" id="pend">
+          Pending
+        </button>
+        <button className="filter-order">All</button>
+        <br />
+        <br />
       </div>
-      
+
       <div id="order-table">
         <table>
           <thead>

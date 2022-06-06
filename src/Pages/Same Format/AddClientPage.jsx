0b3d2d58f95d0012add08f7f96fdfd6a7d953f30
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-06 19:22:44 +0200

    The add client page

--- src/Pages/Same Format/AddClientPage.jsx
+++ src/Pages/Same Format/AddClientPage.jsx
@@ -1,84 +1,62 @@
 import React from "react";
 
 export default function AddClient() {
-  const handleforms = (e) => {
+  const handleforms=(e)=>{
     e.preventDefault();
-  };
+  }
   return (
     <form onSubmit={handleforms} className="flex flex-col  w-[50%] h-[100%]">
-      <h1 className="client text-orange-400 ml-[50rem] mt-[5rem] text-4xl">
-        Client
-      </h1>
-      <div className="client-content ml-[40rem] mt-[5rem]">
-        <h1 className="clients text-2xl w-[15rem]">Client</h1>
-        <input
-          type="text"
-          className="client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4"
-          placeholder="Choose category"
-        />
+      <h1 className='client text-orange-400 ml-[50rem] mt-[5rem] text-4xl'>Client</h1>
+      <div className='client-content ml-[40rem] mt-[5rem]'>
+        <h1 className='clients text-2xl w-[15rem]'>Client</h1>
+        <input type='text' className='client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4' placeholder='Choose category'/>
+        
+
       </div>
-      <div className="client-content ml-[40rem] mt-[5rem]">
-        <h1 className="clients text-2xl w-[15rem]">Category</h1>
-        <input
-          type="text"
-          className="client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4"
-          placeholder="Client name"
-        />
+      <div className='client-content ml-[40rem] mt-[5rem]'>
+        <h1 className='clients text-2xl w-[15rem]'>Category</h1>
+        <input type='text' className='client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4' placeholder='Client name'/>
+
       </div>
-      <div className="client-content ml-[40rem] mt-[5rem]">
-        <h1 className="clients text-2xl w-[15rem]">Represetation</h1>
-        <input
-          type="text"
-          className="client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4"
-          placeholder="full names"
-        />
+      <div className='client-content ml-[40rem] mt-[5rem]'>
+        <h1 className='clients text-2xl w-[15rem]'>Represetation</h1>
+        <input type='text' className='client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4' placeholder='full names'/>
+        
+
       </div>
-      <div className="client-content ml-[40rem] mt-[5rem]">
-        <h1 className="clients text-2xl w-[15rem]">Date of creation</h1>
-        <input
-          type="text"
-          className="client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4"
-          placeholder="Month and a year"
-        />
+      <div className='client-content ml-[40rem] mt-[5rem]'>
+        <h1 className='clients text-2xl w-[15rem]'>Date of creation</h1>
+        <input type='text' className='client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4' placeholder='Month and a year'/>
+        
+
       </div>
-      <div className="client-content ml-[40rem] mt-[5rem]">
-        <h1 className="clients text-2xl w-[15rem]">Address</h1>
-        <input
-          type="text"
-          className="client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4"
-          placeholder="Province,District and sector"
-        />
+      <div className='client-content ml-[40rem] mt-[5rem]'>
+        <h1 className='clients text-2xl w-[15rem]'>Address</h1>
+        <input type='text' className='client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4' placeholder='Province,District and sector'/>
+        
+
       </div>
-      <div className="client-content ml-[40rem] mt-[5rem]">
-        <h1 className="clients text-2xl w-[15rem]">Email</h1>
-        <input
-          type="text"
-          className="client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4"
-          placeholder="Email adress"
-        />
+      <div className='client-content ml-[40rem] mt-[5rem]'>
+        <h1 className='clients text-2xl w-[15rem]'>Email</h1>
+        <input type='text' className='client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4' placeholder='Email adress'/>
+        
+
       </div>
-      <div className="client-content ml-[40rem] mt-[5rem]">
-        <h1 className="clients text-2xl w-[15rem]">Phone number</h1>
-        <input
-          type="text"
-          className="client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4"
-          placeholder="phone number"
-        />
+      <div className='client-content ml-[40rem] mt-[5rem]'>
+        <h1 className='clients text-2xl w-[15rem]'>Phone number</h1>
+        <input type='text' className='client-input text-400xl border-b-2 border-solid py-2 w-[30rem] none mt-4' placeholder='phone number'/>
+        
+
       </div>
-      <div className="client-content ml-[40rem] mt-[5rem]">
-        <h1 className="clients text-2xl w-[15rem]">Bank Account(IBAN)</h1>
-        <input
-          type="text"
-          className="client-input none text-400xl border-b-2 border-solid py-2 w-[30rem]  mt-4"
-          placeholder="Account number"
-        />
+      <div className='client-content ml-[40rem] mt-[5rem]'>
+        <h1 className='clients text-2xl w-[15rem]'>Bank Account(IBAN)</h1>
+        <input type='text' className='client-input none text-400xl border-b-2 border-solid py-2 w-[30rem]  mt-4' placeholder='Account number'/>
+        
+
       </div>
-      <button
-        type="submit"
-        className="btn bg-[orange] cursor-pointer hover:bg-orange-400 p-4 w-48 ml-[50rem] text-white text-2xl rounded-[5px] my-8"
-      >
-        Add client
-      </button>
-    </form>
+      <button type='submit' className='btn bg-[orange] cursor-pointer hover:bg-orange-400 p-4 w-48 ml-[50rem] text-white text-2xl rounded-[5px] my-8'>Add client</button>
+    
+      </form>
+ 
   );
 }
